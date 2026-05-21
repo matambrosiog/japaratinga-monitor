774aaae96935f1fc788dfaaa382577fa2552cf56
matambrosiog <mat_ambrosio@hotmail.com>
2026-05-20 22:58:48 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,19 +1,58 @@
-import os
-from telegram import Bot
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
-print("Iniciando script...")
+import requests
+import time
+import re
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+URL = "SEU_LINK_DA_OMNIBEES"
 
-print("TOKEN:", TOKEN)
-print("CHAT_ID:", CHAT_ID)
+TELEGRAM_TOKEN = "TOKEN"
+CHAT_ID = "CHAT_ID"
 
-bot = Bot(token=TOKEN)
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
-bot.send_message(
-    chat_id=CHAT_ID,
-    text="🚀 TESTE DIRETO DO GITHUB ACTIONS"
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=options
 )
 
-print("Mensagem enviada com sucesso.")
+driver.get(URL)
+
+time.sleep(10)
+
+texto = driver.page_source
+
+driver.quit()
+
+precos = re.findall(r'R\\$\\s?[\\d\\.]+,\\d{2}', texto)
+
+if precos:
+    menor_preco = precos[0]
+
+    mensagem = f"""
+🏨 Japaratinga Monitor
+
+💰 Menor preço encontrado:
+{menor_preco}
+
+🔗 {URL}
+"""
+
+    requests.post(
+        f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage",
+        data={
+            "chat_id": CHAT_ID,
+            "text": mensagem
+        }
+    )
+
+    print("Mensagem enviada!")
+else:
+    print("Preço não encontrado")
